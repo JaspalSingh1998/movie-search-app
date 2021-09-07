9f94c2c81de71941ba5c6fe1d97caea93a313ecd
Jaspal Singh <jaspalsinghpersonal9@gmail.com>
2021-09-07 08:22:15 +0530

    Added API to fetch single movie details

--- movie.js
+++ movie.js
@@ -1,1 +1,16 @@
-const MOVIE = `https://api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=en-US`;
+const API_KEY = "25210924b2f5041e4ce23097c2cadaea";
+
+async function getId() {
+  const movieId = await localStorage.getItem("MOVIE_ID");
+  getMovie(movieId);
+}
+
+async function getMovie(id) {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`
+  );
+  const { title, tagline, release_date, overview, revenue } =
+    await response.json();
+}
+
+getId();
